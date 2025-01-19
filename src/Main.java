f7aa991182a7d9c849255233904e1722ff7024ed
Teddy-38 <Zhdanov_as94@mail.ru>
2025-01-19 20:07:37 +0800

    Task2

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,16 @@ public class Main {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
         System.out.println("Task 2");
-
+        int clientOSYear = 1;
+        int clientDeviceYear = 2015;
+        if (clientOSYear == 0 && clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOSYear == 0 && clientDeviceYear > 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOSYear == 1 && clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
     }
 }
