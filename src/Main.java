35f6234dd4aa351ab9e83a59a2291c196d3e0fc4
Teddy-38 <Zhdanov_as94@mail.ru>
2025-01-19 19:34:56 +0800

    Task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Task 1");
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        System.out.println("Task 2");
+
     }
 }
